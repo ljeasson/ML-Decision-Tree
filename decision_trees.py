Fix bugs with H and IG calculation

decision_trees.py
@@ -66,48 +66,46 @@ def calculate_entropy(Y):
     return entropy
 
 def calculate_feature_entropy(feature, labels, binary_value):
-    # Count of 0s and 1s respectively
+    H_0 = 0 
+    H_1 = 0
     num_0 = 0
     num_1 = 0
-    # Proportion of 0s and 1s respectively
     prop_0 = 0
-    prop_1 = 1
-
+    prop_1 = 0 
+    
     if binary_value == 0:
         # Calculate H(Feature == 0)
-        H_0 = 0  
-
         No = np.where(feature == 0)[0]
+
         for i in No:
-            if feature[i] == labels[i]: num_0 += 1
+            if feature[i] != labels[i]: num_0 += 1
             else: num_1 += 1
+
         if (len(No) == 0): return H_0
         else:    
-            prop_0 = num_0 / len(No) 
+            prop_0 = num_0 / len(No)
             prop_1 = num_1 / len(No)
 
-        if prop_0 == 1 or prop_1 == 1:
-            H_0 = 0     
+        if prop_0 == 1 or prop_1 == 1: H_0 = 0     
         else: H_0 = -(prop_0 * math.log2(prop_0)) + -(prop_1 * math.log2(prop_1))
 
         return H_0
 
     else:
         # Calculate H(Feature == 1)
-        H_1 = 0
-
         Yes = np.where(feature == 1)[0]
+
         for i in Yes:
-            if feature[i] == labels[i]: num_0 += 1
+            if feature[i] != labels[i]: num_0 += 1
             else: num_1 += 1
+
         if (len(Yes) == 0): return H_1
         else:
-            prop_0 = num_0 / len(Yes) 
+            prop_0 = num_0 / len(Yes)
             prop_1 = num_1 / len(Yes)
 
-        if prop_0 == 1 or prop_1 == 1:
-            H_1 = 0
-        else: H_1 = -(prop_0 * math.log(prop_0)) + -(prop_1 * math.log(prop_1))
+        if prop_0 == 1 or prop_1 == 1: H_1 = 0
+        else: H_1 = -(prop_0 * math.log2(prop_0)) + -(prop_1 * math.log2(prop_1))
 
         return H_1
 
@@ -129,7 +127,7 @@ def DT_train_binary(X, Y, max_depth):
 
     # Create numpy array for Labels
     labels = np.zeros(shape=(num_samples))
-    for sample in Y: labels[sample] = Y[sample]
+    for sample in range(len(Y)): labels[sample] = Y[sample]
 
     # GREEDY ALGORITHM
 
@@ -146,42 +144,35 @@ def DT_train_binary(X, Y, max_depth):
 
         # Iterate through the sample in the current feature
         # adding each sample to current feature numpy array
-        for sample in range(num_samples):
-            current_feature[sample] = X[sample][feature]
-        print(current_feature)
+        for sample in range(num_samples): current_feature[sample] = X[sample][feature]
 
         # Calculate entropy of the split
-        # where feature is 0
+        # where feature is 0 and 1 respectively
         H_0 = calculate_feature_entropy(current_feature, labels, 0)
-        print("H_0:",H_0)
-        # where feature is 1
         H_1 = calculate_feature_entropy(current_feature, labels, 1)
-        print("H_1:",H_1)
 
-        # Calculate proportion of 0s and 1s in labels
-        prop_labels_0 = int(np.count_nonzero(labels == 0)) / num_samples
-        print("Prop_0:",prop_labels_0)
-        prop_labels_1 = int(np.count_nonzero(labels == 1)) / num_samples
-        print("Prop_1:",prop_labels_1)
+        # Calculate proportion of 0s and 1s in feature
+        prop_features_0 = int(np.count_nonzero(current_feature == 0)) / num_samples
+        prop_features_1 = int(np.count_nonzero(current_feature == 1)) / num_samples
 
         # Calculate IG for current feature
-        IG = H - ( (prop_labels_0 * H_0) + (prop_labels_1 * H_1) )
-        print("IG:",IG)
-        print()
+        IG = H - ( (prop_features_0 * H_0) + (prop_features_1 * H_1) )
 
         # Update feature IG dictionary and current index
         Feature_IG[current_index] = IG
         current_index += 1
 
-    # Find feature with maximum IG
     print(Feature_IG)
+
+    # Find feature with maximum IG
     Max_IG = max(Feature_IG.values())
     Max_feature = [k for k,v in Feature_IG.items() if v == Max_IG]
-    print(Max_feature, Max_IG)
-    print()
 
     # Add feature with maximum IG to DT
     DT.add("F"+str(Max_feature), Max_IG)
+
+    # If IG = 0, stop
+    # else, continue
 
     # Return DT as list of lists, numpy array, or class object
     return DT
@@ -205,16 +196,60 @@ def DT_train_real_best(X_train, Y_train, X_val, Y_val):
 
 # Test Data
 '''
+Test 1
 F1 F2 F3 F4  L
 0  1  0  1 | 1
 1  1  1  1 | 1
 0  0  0  1 | 0
+
+Training Set 1
+F1 F2   L
+0  1  | 1
+0  0  | 0
+1  0  | 0
+0  0  | 0
+1  1  | 1
+
+Training Set 2
+F1 F2 F3 F4  L
+0  1  0  0 | 0
+0  0  0  1 | 1
+1  0  0  0 | 0
+0  0  1  1 | 0
+1  1  0  1 | 1
+1  1  0  0 | 0
+1  0  0  1 | 1
+0  1  0  1 | 1
+0  1  0  0 | 1
+
+Trainin Set Example
+    F1 F2 F3 F4  L
+1   1  1  0  0 | 0
+2   1  1  1  1 | 1
+3   1  1  1  1 | 1
+4   0  0  0  1 | 0
+5   0  0  1  1 | 0
+6   0  0  1  0 | 1
+7   0  0  0  0 | 0
+8   1  0  1  0 | 0
+9   1  1  1  0 | 1
+10  0  0  1  1 | 0
 '''
-training_features = np.array([ [0,1,0,1], [1,1,1,1], [0,0,0,1] ])
-training_labels = np.array([ [1], [1], [0] ])
+training_features_1 = np.array([ [0, 1, 0, 1], [1, 1, 1, 1], [0, 0, 0, 1] ])
+training_labels_1 = np.array([ [1], [1], [0] ])
+
+training_features_2 = np.array([ [0,1], [0,0], [1,0], [0,0], [1,1] ])
+training_labels_2 = np.array([ [1], [0], [0], [0], [1] ])
+
+training_features_3 = np.array([ [0,1,0,0], [0,0,0,1], [1,0,0,0], [0,0,1,1], [1,1,0,1], [1,1,0,0], [1,0,0,1], [0,1,0,1], [0,1,0,0] ])
+training_labels_3 = np.array([ [0], [1], [0], [0], [1], [0], [1], [1], [1] ])
+
+
+X = np.array([ [1,1,0,0], [1,1,1,1], [1,1,1,1], [0,0,0,1], [0,0,1,1], [0,0,1,0], [0,0,0,0], [1,0,1,0], [1,1,1,0], [0,0,1,1] ])
+Y = np.array([ [0], [1], [1], [0], [0], [1], [0], [0], [1], [0] ])
 max_depth = 2
 
 # Test DT_train_binary() and DT_test_binary()
-DT = DT_train_binary(training_features, training_labels, max_depth)
+DT = DT_train_binary(X, Y, max_depth)
 DT.print_tree()
-test_acc = DT_test_binary(training_features, training_labels, DT)
+test_acc = DT_test_binary(training_features_1, training_labels_1, DT)