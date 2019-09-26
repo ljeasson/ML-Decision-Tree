7ab7ee1fb7cba70aeddc21b518ad4725f13c961f
kurtisrodrigue <42788967+kurtisrodrigue@users.noreply.github.com>
2019-09-25 22:40:25 -0700

    Final Compilation
    Combined files into one, took out testing lines that were copied into test.py in previous commit.

--- decision_trees.py
+++ decision_trees.py
@@ -43,6 +43,31 @@ class Binary_Tree:
             print(str(node.value) + ' ' + str(node.IG))
             self.print_nodes(node.yes)
 
+class Real_Node:
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.value = 0
+        self.feature = 0
+        self.depth = 0
+        self.left_prediction = 0
+        self.right_prediction = 1
+
+
+class Real_Binary_Tree:
+    def __init__(self):
+        self.root = None
+
+    def print_tree(self):
+        if self.root is not None:
+            self.print_nodes(self.root)
+
+    def print_nodes(self, node):
+        if node is not None:
+            self.print_nodes(node.left)
+            print(str(node.value) + ' ' + str(node.feature) + ' ' + str(node.depth) + ' ' + str(node.left_prediction) + ' ' + str(node.right_prediction))
+            self.print_nodes(node.right)
+
 def calculate_entropy(Y):
     # Set of all labels
     labels = (0, 1)
@@ -56,35 +81,49 @@ def calculate_entropy(Y):
     # Calculate proportion of each label
     prop_labels_0 = num_labels_0 / length
     prop_labels_1 = num_labels_1 / length
+
+    H0 = 0
+    H1 = 0
+
+    if prop_labels_0 > 0:
+        H0 = -1 * prop_labels_0 * math.log2(prop_labels_0)
+
+    if prop_labels_1 > 0:
+        H1 = -1 * prop_labels_1 * math.log2(prop_labels_1)
     
     # Calculate entropy
-    entropy = (-1 * prop_labels_0 * math.log2(prop_labels_0)) \
-            + (-1 * prop_labels_1 * math.log2(prop_labels_1))
+    entropy = H0 + H1
     return entropy
 
+
 def calculate_feature_entropy(feature, labels, binary_value):
-    H_0 = 0 
+    H_0 = 0
     H_1 = 0
     num_0 = 0
     num_1 = 0
     prop_0 = 0
-    prop_1 = 0 
-    
+    prop_1 = 0
+
     if binary_value == 0:
         # Calculate H(Feature == 0)
         No = np.where(feature == 0)[0]
 
         for i in No:
-            if feature[i] != labels[i]: num_0 += 1
-            else: num_1 += 1
+            if feature[i] != labels[i]:
+                num_0 += 1
+            else:
+                num_1 += 1
 
-        if (len(No) == 0): return H_0
-        else:    
+        if (len(No) == 0):
+            return H_0
+        else:
             prop_0 = num_0 / len(No)
             prop_1 = num_1 / len(No)
 
-        if prop_0 == 1 or prop_1 == 1: H_0 = 0     
-        else: H_0 = -(prop_0 * math.log2(prop_0)) + -(prop_1 * math.log2(prop_1))
+        if prop_0 == 1 or prop_1 == 1:
+            H_0 = 0
+        else:
+            H_0 = -(prop_0 * math.log2(prop_0)) + -(prop_1 * math.log2(prop_1))
 
         return H_0
 
@@ -93,31 +132,37 @@ def calculate_feature_entropy(feature, labels, binary_value):
         Yes = np.where(feature == 1)[0]
 
         for i in Yes:
-            if feature[i] != labels[i]: num_0 += 1
-            else: num_1 += 1
+            if feature[i] != labels[i]:
+                num_0 += 1
+            else:
+                num_1 += 1
 
-        if (len(Yes) == 0): return H_1
+        if (len(Yes) == 0):
+            return H_1
         else:
             prop_0 = num_0 / len(Yes)
             prop_1 = num_1 / len(Yes)
 
-        if prop_0 == 1 or prop_1 == 1: H_1 = 0
-        else: H_1 = -(prop_0 * math.log2(prop_0)) + -(prop_1 * math.log2(prop_1))
+        if prop_0 == 1 or prop_1 == 1:
+            H_1 = 0
+        else:
+            H_1 = -(prop_0 * math.log2(prop_0)) + -(prop_1 * math.log2(prop_1))
 
         return H_1
+
 
 def build_tree(DT, Feature_IG, labels, num_samples):
     # Find feature with maximum IG
     Max_IG = max(Feature_IG.values())
-    Max_feature = [k for k,v in Feature_IG.items() if v == Max_IG]
+    Max_feature = [k for k, v in Feature_IG.items() if v == Max_IG]
 
     # Add feature with maximum IG to DT
-    DT.add("F"+str(Max_feature), Max_IG)
+    DT.add("F" + str(Max_feature), Max_IG)
 
     # Calculate entopy of maximum IG feature
     feature = np.zeros(shape=(num_samples))
-    for sample in range(num_samples): feature[sample] = X[sample][int(Max_feature[0])-1]
-    
+    for sample in range(num_samples): feature[sample] = X[sample][int(Max_feature[0]) - 1]
+
     H_0 = calculate_feature_entropy(feature, labels, 0)
     H_1 = calculate_feature_entropy(feature, labels, 1)
     DT.add("_", H_0)
@@ -125,17 +170,18 @@ def build_tree(DT, Feature_IG, labels, num_samples):
 
     # Remove max key, value pair
     del Feature_IG[Max_feature[0]]
-    
+
     # Recursively build tree as long as Feature_IG dict
     # is not empty
     if len(Feature_IG) > 0:
         build_tree(DT, Feature_IG, labels, num_samples)
 
+
 def DT_train_binary(X, Y, max_depth):
     # Check if X any Y have same number of samples
     # Return 0 if unequal sample number
     if X.shape[0] != Y.shape[0]: return 0
-    
+
     # Number of samples and features in training data
     num_samples = X.shape[0]
     num_features = X.shape[1]
@@ -152,9 +198,9 @@ def DT_train_binary(X, Y, max_depth):
 
     # GREEDY ALGORITHM
 
-    # Initialize dictionary for storing IG 
+    # Initialize dictionary for storing IG
     # of each feature and current index of feature
-    Feature_IG = dict.fromkeys(list(range(1, num_features+1, 1)))
+    Feature_IG = dict.fromkeys(list(range(1, num_features + 1, 1)))
     current_index = 1
 
     # Iterate through features and calculate IG for each
@@ -177,7 +223,7 @@ def DT_train_binary(X, Y, max_depth):
         prop_features_1 = int(np.count_nonzero(current_feature == 1)) / num_samples
 
         # Calculate IG for current feature
-        IG = H - ( (prop_features_0 * H_0) + (prop_features_1 * H_1) )
+        IG = H - ((prop_features_0 * H_0) + (prop_features_1 * H_1))
 
         # Update feature IG dictionary and current index
         Feature_IG[current_index] = IG
@@ -195,9 +241,9 @@ def DT_test_binary(X, Y, DT):
     predictions = []
     for i in range(num_samples):
         print(X[i])
-        #predictions.append(X[i][0])
+        # predictions.append(X[i][0])
         predictions.append(DT_predict(X[i], DT))
-    
+
     diff = 0
     same = 0
     for i in range(len(predictions)):
@@ -209,8 +255,10 @@ def DT_test_binary(X, Y, DT):
     accuracy = same / (same + diff)
     return accuracy
 
+
 def DT_predict(X, DT):
     return DT_predict_recursive(X, DT.get_root())
+
 
 def DT_predict_recursive(X, node):
     if node.no is not None:
@@ -232,15 +280,187 @@ def DT_predict_recursive(X, node):
             return node.right_prediction
     '''
 
+
 def DT_train_binary_best(X_train, Y_train, X_val, Y_val):
     return
 
-# Test DT_train_binary(X, Y, max_depth) and DT_test_binary(X, Y, DT)
-X = np.array([ [1,1,0,0], [1,1,1,1], [1,1,1,1], [0,0,0,1], [0,0,1,1], [0,0,1,0], [0,0,0,0], [1,0,1,0], [1,1,1,0], [0,0,1,1] ])
-Y = np.array([ [0], [1], [1], [0], [0], [1], [0], [0], [1], [0] ])
-max_depth = 2
 
-DT = DT_train_binary(X, Y, max_depth)
-DT.print_tree()
-test_acc = DT_test_binary(X, Y, DT)
-print("Accuracy:",test_acc)
+def DT_train_real(X, Y, max_depth):
+    # Check if X any Y have same number of rows
+    # Return 0 if unequal row number
+    if X.shape[0] != Y.shape[0]:
+        return 0
+
+
+    # Create new Binary Tree for DT
+    DT = Real_Binary_Tree()
+    # Calculate entropy of entire dataset
+    H = calculate_entropy(Y)
+    # Split training data at each feature
+    DT.root = Real_Node()
+    DT.root.depth = 1
+    Y = Y.flatten()
+    samples = []
+    for i in range(Y.shape[0]):
+        samples.append(i)
+    # Call recursive function
+    DT_train_real_recursive(DT.root, samples, X, Y, max_depth, H)
+
+    # Return DT as list of lists, numpy array, or class object
+    return DT
+
+def DT_train_real_recursive(node, sample_nums, X, Y, max_depth, H):
+    if node.depth > max_depth:
+        if max_depth != -1:
+            return
+
+    IG = []
+    best_split = {}
+
+    # Loop through features, calculate best IG
+    for i in range(X.shape[1]):
+        temp_IG = []
+
+        for x in sample_nums:
+            samples_0 = []
+            samples_1 = []
+            labels_0 = []
+            labels_1 = []
+            value = X[x][i]
+            for y in sample_nums:
+                if X[y][i] <= value:
+                    samples_0.append(X[y][i])
+                    labels_0.append(Y[y])
+                else:
+                    samples_1.append(X[y][i])
+                    labels_1.append(Y[y])
+
+            # Calculate probability on each split
+            p_L = len(samples_0) / len(sample_nums)
+            p_R = len(samples_1) / len(sample_nums)
+
+            # Calculate entropy after split at each side
+            l_entropy = 0
+            if len(labels_0) > 0:
+                labels_0 = np.array(labels_0)
+                l_entropy = calculate_entropy(labels_0)
+
+            r_entropy = 0
+            if len(labels_1) > 0:
+                labels_1 = np.array(labels_1)
+                r_entropy = calculate_entropy(labels_1)
+
+            temp_IG.append(H - (p_L * l_entropy) - (p_R * r_entropy))
+
+        best = 0
+        for z in range(len(temp_IG)):
+            if temp_IG[z] > temp_IG[best]:
+                best = z
+        # dict feature# -> best sample to split on
+        best_split[i] = best
+        IG.append(temp_IG[best])
+
+
+    # Choose best feature to split on based off IG
+    feature = 0
+    for i in range(len(IG)):
+        if IG[i] > IG[feature]:
+            feature = i
+
+    # name feature for node
+    node.feature = feature
+    value = X[sample_nums[best_split[feature]]][feature]
+    node.value = value
+    left_samples = []
+    right_samples = []
+    right_labels = []
+    left_labels = []
+    for i in sample_nums:
+        if X[i][feature] <= value:
+            left_samples.append(i)
+            left_labels.append(Y[i])
+        else:
+            right_samples.append(i)
+            right_labels.append(Y[i])
+
+    # Create new nodes and give them the appropriate samples and depth
+    test = 0
+    if len(left_labels) >= 1:
+        test = left_labels[0]
+        same = True
+        for i in range(len(left_labels)):
+            if left_labels[i] != test:
+                same = False
+        if same:
+            node.left_prediction = left_labels[0]
+        else:
+            node.left = Real_Node()
+            node.left.depth = node.depth + 1
+            DT_train_real_recursive(node.left, left_samples, X, Y, max_depth, H)
+
+    if len(right_labels) >= 1:
+        test = right_labels[0]
+        same = True
+        for i in range(len(right_labels)):
+            if right_labels[i] != test:
+                same = False
+        if same:
+            node.right_prediction = right_labels[0]
+        else:
+            node.right = Real_Node()
+            node.right.depth = node.depth + 1
+            DT_train_real_recursive(node.right, right_samples, X, Y, max_depth, H)
+
+    if len(right_samples) == 0:
+        node.right_prediction = not node.left_prediction
+    elif len(left_samples) == 0:
+        node.left_prediction = not node.right_prediction
+    return
+
+def DT_test_real(X, Y, DT):
+    predictions = []
+    for i in range(X.shape[0]):
+        predictions.append(DT_predict_real(X[i], DT.root))
+
+    diff = 0
+    same = 0
+    for i in range(len(predictions)):
+        if predictions[i] == Y[i]:
+            same += 1
+        else:
+            diff += 1
+
+    accuracy = same / (same + diff)
+    return accuracy
+
+def DT_predict_real(X, DT):
+    return DT_predict_real_recursive(X, DT.root)
+
+def DT_predict_real_recursive(X, node):
+    if X[node.feature] <= node.value:
+        if node.left:
+            DT_predict_real(X, node.left)
+        else:
+            return node.left_prediction
+    else:
+        if node.right:
+            DT_predict_real(X, node.right)
+        else:
+            return node.right_prediction
+    return
+
+def DT_train_real_best(X_train, Y_train, X_val, Y_val):
+    DTs = []
+    i = 0
+    # make unique trees, iterative depth
+    while True:
+        # train a new tree of depth i
+        DTs.append(DT_train_real(X_train, Y_train, i))
+        i += 1
+        # test accuracy of tree
+        if DT_test_real(X_val, Y_val, DTs[i-1]) == 1:
+            return DTs[i-1]
+        # if the accuracy does not change with an increase in depth, return the tree
+        if len(DTs) > 1:
+            if DT_test_real(X_val, Y_val, DTs[i-1]) == DT_test_real(X_val, Y_val, DTs[i-2]):
+                return DTs[i-1]
